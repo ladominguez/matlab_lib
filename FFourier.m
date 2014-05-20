2125d5560c08170158452bf85280805dfa1239e4
Toronto <antonio@toronto.local>
2014-05-20 16:14:51 -0500

    FFourier modified. The new files accepts either structures, or arrays.

--- FFourier.m
+++ FFourier.m
@@ -1,6 +1,14 @@
 function [f X]=FFourier(x,Fs)
 % [f X]=FFourier(x,Fs)
 
+if isstruct(x)
+    Fs = 1/x.dt;
+    x  = x.d;
+end
+    
+    
+    
+
 T=1/Fs;
 L=length(x);
 NFFT=2^nextpow2(L);
@@ -11,9 +19,10 @@ f=Fs/2*linspace(0,1,NFFT/2+1);
 if nargout==0
     subplot(2,1,1)
     semilogy(f,2*abs(X))
-    %loglog(f,2*abs(X))
+    loglog(f,2*abs(X))
     xlabel('Frequency Hz')
     ylabel('2*abs(X)')
+    xlim([0.0 10])
     grid
     subplot(2,1,2)
     plot((0:L-1).*T,x)
